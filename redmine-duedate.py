0a870bacf9eeec8447e43beba0fde30e7fcf0f24
Eugene Mogilevsky <ujin77@i.ua>
2018-06-05 12:59:44 +0300

    Add config

--- redmine-duedate.py
+++ redmine-duedate.py
@@ -7,10 +7,37 @@ import os
 import sys
 import mysql.connector
 from mysql.connector import errorcode
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import ConfigParser
+import json
 
 
 PROG = os.path.basename(sys.argv[0]).rstrip('.py')
 PROG_DESC = 'Send notification for redmine issue due date'
+
+DEFAULT_CONFIG = {
+    'name': PROG,
+    'mail': {
+        'to':   'test@domain.local',
+        'from': 'test@domain.local',
+        'host': 'smtp.domain.local',
+        'user': 'test@domain.local',
+        'password': 'password',
+        'port': 465,
+        'subject': 'TEST'
+    },
+    'redmine': {
+        'host': 'localhost',
+        'password': 'password',
+        'user': 'user',
+        'database': 'redmine'
+    }
+}
+
+QUERY_HOSTNAME = "SELECT `value` FROM `settings` WHERE `name` = 'host_name'"
+QUERY_PROTOCOL = "SELECT `value` FROM `settings` WHERE `name` = 'protocol'"
 
 QUERY_24X7 = """
 SELECT `issues`.`id`,
@@ -18,7 +45,7 @@ SELECT `issues`.`id`,
 `issue_statuses`.`name` AS 'Status',
 `custom_values`.`value` AS 'SLA',
 `issues`.`created_on` AS 'Created',
-DATE_ADD(`issues`.`created_on`, INTERVAL 2 HOUR) AS 'sla_due_time',
+DATE_ADD(`issues`.`created_on`, INTERVAL 2 HOUR) AS 'SLA_due_time',
 `issues`.`due_date`
 FROM `issues`, `issue_statuses`, `custom_values`, `custom_fields`
 WHERE
@@ -33,7 +60,7 @@ SELECT `issues`.`id`,
 `issue_statuses`.`name` AS 'Status',
 `custom_values`.`value` AS 'SLA',
 `issues`.`created_on` AS 'Created',
-DATE_ADD(`issues`.`created_on`, INTERVAL 1 DAY) AS 'sla_due_time',
+DATE_ADD(`issues`.`created_on`, INTERVAL 1 DAY) AS 'SLA_due_time',
 `issues`.`due_date`
 FROM `issues`, `issue_statuses`, `custom_values`, `custom_fields`
 WHERE
@@ -43,43 +70,60 @@ WHERE
 DATE_ADD(`issues`.`created_on`, INTERVAL 1 DAY) < NOW()
 """
 
+HTML_STYLE = """
+<style>
+table { border-collapse: collapse; }
+th, td {
+    border: 1px solid gray;
+    padding-right: 6px;
+    padding-left: 6px;
+}
+a {text-decoration: none}
+</style>
+"""
+
+HTML_FORMAT = """
+<html>
+<head>
+{STYLE}
+</head>
+<body>
+{DATA}
+</body></html>
+"""
+EMAIL_FORMAT = 'From: {}\r\nTo: {}\r\nSubject: {}\r\n\r\n{}'
 QUERY_HEAD_FORMAT = u'| {:>6s} | {:25.25s} | {:20.20s} | {:5} | {:16s} | {:16s} |'
 QUERY_DATA_FORMAT = u'| {:6d} | {:25.25s} | {:20.20s} | {:5} | {:%Y-%m-%d %H:%M} | {:%Y-%m-%d %H:%M} |'
+REPORT_HEAD = u'<TABLE>\n<TR><TD>{}</TD><TD>{}</TD><TD>{}</TD><TD>{}</TD><TD>{}</TD></TR>\n'
+REPORT_DATA = u"""<TR>
+<TD><A href={url}{id}>{issue} <b>#{id}</b></A></TD>
+<TD>{status}</TD>
+<TD>{sla}</TD>
+<TD>{created}</TD>
+<TD>{duetime}</TD>
+</TR>\n"""
+REPORT_FOOT = u'</TABLE>\n'
+
 QUERY_REPORT = """%s
 UNION
 %s
 ORDER BY `SLA`, `Created`
 """ % (QUERY_24X7, QUERY_5X8)
 
-def send_notifications(config):
-    print("Send")
-    try:
-        cnx = mysql.connector.connect(**config)
-    except mysql.connector.Error as err:
-        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print("Something is wrong with your user name or password")
-        elif err.errno == errorcode.ER_BAD_DB_ERROR:
-            print("Database does not exist")
-        else:
-            print(err)
-    else:
-        cursor = cnx.cursor()
-        cursor.execute(SELECT_5x8)
-        for data in cursor:
-            print data
-        cursor.close()
-        cnx.close()
+# QUERY_REPORT = QUERY_24X7
 
 
 class RmClient(object):
 
     cnx = None
     cursor = None
+    issue_url = ''
 
-    def __init__(self, my_config):
+    def __init__(self, _config):
         super(RmClient, self).__init__()
+        self._config = _config
         try:
-            self.cnx = mysql.connector.connect(**my_config)
+            self.cnx = mysql.connector.connect(**self._config['redmine'])
         except mysql.connector.Error as err:
             if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
                 print("Something is wrong with your user name or password")
@@ -89,6 +133,7 @@ class RmClient(object):
                 print(err)
         else:
             self.cursor = self.cnx.cursor()
+            self._get_url()
 
     def close(self):
         if self.cursor:
@@ -96,35 +141,95 @@ class RmClient(object):
         if self.cnx:
             self.cnx.close()
 
-    def send_notifications(self):
-        print("Send")
-        self.cursor.execute(QUERY_REPORT)
-        print(QUERY_HEAD_FORMAT.format('ID', 'Subject', 'Status', 'SLA', 'Created', 'Due time'))
-        for data in self.cursor:
-            print(QUERY_DATA_FORMAT.format(data[0], data[1], data[2], data[3], data[4], data[5]))
+    def _get_url(self):
+        self.cursor.execute(QUERY_HOSTNAME)
+        host = ''.join(self.cursor.fetchone())
+        self.cursor.execute(QUERY_PROTOCOL)
+        proto = ''.join(self.cursor.fetchone())
+        self.issue_url = "%s://%s/issues/" % (proto, host)
 
+    def send_notifications(self):
+        result = False
+        self.cursor.execute(QUERY_REPORT)
+        message = REPORT_HEAD.format(
+                                     self.cursor.column_names[1],
+                                     self.cursor.column_names[2],
+                                     self.cursor.column_names[3],
+                                     self.cursor.column_names[4],
+                                     self.cursor.column_names[5]
+                                     )
+        for data in self.cursor:
+            result = True
+            message += REPORT_DATA.format(url=self.issue_url,
+                                          id=data[0],
+                                          issue=data[1],
+                                          status=data[2],
+                                          sla=data[3],
+                                          created=data[4],
+                                          duetime=data[5]
+                                          )
+            if self._config['verbose']:
+                print(QUERY_DATA_FORMAT.format(data[0],
+                                     data[1],
+                                     data[2],
+                                     data[3],
+                                     data[4],data[5]))
+
+        if result:
+            message += REPORT_FOOT
+            self._send_mail(message.encode('utf-8'))
+        else:
+            if self._config['verbose']:
+                print('No issues')
+
+    def _send_mail(self, message):
+        to_addr_list = self._config['mail']['to'].split(',')
+        print(to_addr_list)
+        # return
+        html = HTML_FORMAT.format(STYLE=HTML_STYLE, DATA=message)
+        msg = MIMEMultipart('alternative', None, [MIMEText(html, 'html','utf-8')])
+        msg['Subject'] = self._config['mail']['subject']
+        msg['From'] = self._config['mail']['from']
+        msg['To'] = self._config['mail']['to']
+        server = smtplib.SMTP_SSL(self._config['mail']['host'],self._config['mail']['port'])
+        if self._config['debug']:
+            server.set_debuglevel(1)
+        server.login(self._config['mail']['user'], self._config['mail']['password'])
+        server.sendmail(self._config['mail']['from'], self._config['mail']['to'].split(','), msg.as_string())
+        server.quit()
+
+def load_config(fname):
+    if os.path.isfile(fname):
+        config = ConfigParser.ConfigParser(allow_no_value=True)
+        try:
+            config.readfp(open(fname))
+            for section in config.sections():
+                for (name, value) in config.items(section):
+                    if not DEFAULT_CONFIG.get(section): DEFAULT_CONFIG[section]={}
+                    DEFAULT_CONFIG[section][name] = value.strip("'\"")
+        except ConfigParser.MissingSectionHeaderError as e:
+            print e
+        except Exception as e:
+            print e
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=PROG_DESC)
-    parser.add_argument('-r', '--redmine')
-    parser.add_argument('-u', '--username')
-    parser.add_argument('-p', '--password')
-    parser.add_argument('-d', '--database', default='redmine')
+    parser.add_argument('-c', '--config', default='/etc/'+ PROG +'.conf')
     parser.add_argument('-f', '--fix', action='store_true', help="Fix the due date")
     parser.add_argument('-s', '--send', action='store_true', help="Send notifications")
-
+    parser.add_argument('-d', '--debug', action='store_true', help="Debug output")
+    parser.add_argument('-v', '--verbose', action='store_true', help="Verbose output")
     args = parser.parse_args()
 
-    if args.redmine and args.username and args.password:
-        config = {
-            'user': args.username,
-            'password': args.password,
-            'host': args.redmine,
-            'database': args.database,
-            'raise_on_warnings': True,
-        }
-        client=RmClient(config)
+    if args.config: load_config(args.config)
+    DEFAULT_CONFIG['verbose'] = args.verbose
+    DEFAULT_CONFIG['debug'] = args.debug
+    if args.debug:
+        print 'CONFIG:', json.dumps(DEFAULT_CONFIG, indent=2)
+
+    if args.fix or args.send:
+        client = RmClient(DEFAULT_CONFIG)
         if args.fix:
             pass
         if args.send:
@@ -133,4 +238,3 @@ if __name__ == "__main__":
         client.close()
     else:
         parser.print_help()
-
